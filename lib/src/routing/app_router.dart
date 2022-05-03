706239c57b96b8fd43aed76020c4d90893089052
Micaela Piacenza <micaela@inthecode.com.au>
2022-05-04 08:06:39 +1000

    E-commerce app up to 2.6(go-router) (26% completed)

---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,38 @@
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../features/account/account_screen.dart';
+import '../features/orders_list/orders_list_screen.dart';
+import '../features/products_list/products_list_screen.dart';
+import '../features/shopping_cart/shopping_cart_screen.dart';
+
+final goRouter = GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsListScreen(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            pageBuilder: (context, state) => MaterialPage(child: const ShoppingCartScreen(), fullscreenDialog: true, key: state.pageKey)
+          ),
+          GoRoute(
+            path: 'orders',
+            pageBuilder: (context, state) => MaterialPage(child: const OrdersListScreen(), fullscreenDialog: true, key: state.pageKey)
+          ),
+          GoRoute(
+            path: 'account',
+            pageBuilder: (context, state) => MaterialPage(child: const AccountScreen(), fullscreenDialog: true, key: state.pageKey)
+          ),
+          GoRoute(
+            path: 'signIn',
+            pageBuilder: (context, state) => MaterialPage(child: const EmailPasswordSignInScreen(formType: EmailPasswordSignInFormType.signIn,), fullscreenDialog: true, key: state.pageKey)
+          ),
+        ]
+      ),
+    ]
+);
